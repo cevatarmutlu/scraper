a0680182ec0a883115dac03d726b3c1844f19aec
cevatarmutlu <cevatarmutlu@gmail.com>
2021-10-27 11:28:48 +0300

    add comment lines to google_sheet.py

--- src/google_sheet.py
+++ src/google_sheet.py
@@ -1,16 +1,17 @@
-from __future__ import print_function
 import os.path
-from typing import List
+from typing import List, Union
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from datetime import datetime
 import logging
 
 logger = logging.getLogger(__name__)
 
 class GoogleSheet:
+    """
+        This class creates Google Spreadsheet and append data the file.
+    """
     def __init__(self) -> None:
         self.scopes = ['https://www.googleapis.com/auth/spreadsheets']
         self.creds = None
@@ -18,6 +19,9 @@ class GoogleSheet:
         self.spreadsheet = None
     
     def create_service(self):
+        """
+            Configurations for Google Spreadsheet operations
+        """
         if os.path.exists('token.json'):
             self.creds = Credentials.from_authorized_user_file('token.json', self.scopes)
 
@@ -36,35 +40,55 @@ class GoogleSheet:
 
         return service
     
-    def create_sheet(self, sheet_name, rows: List = ["URL", "Product Code", "ProductName", "Availability", "Offer", "Product Price", "Sale Price"]):
-        current_time = datetime.now().strftime("%Y-%m-%d-%H")
+    def create_sheet(self, spreadsheet_name: str, header: Union[List, None] = ["URL", "Product Code", "ProductName", "Availability", "Offer", "Product Price", "Sale Price"]) -> None:
+        """
+            Create Spreadsheet file and write header.
+
+            Args:
+                spreadsheet_name(str): File name
+                header(List | None): Specifies whether there is header row in the Spreadsheet file. If header variable is None there is no header in the Spreadsheet file otherwise List of columns names.
+
+            Returns:
+                None
+        """
+
+        assert type(spreadsheet_name) == str, "spreadsheet_name variable must be str."
+        assert type(header) == list or header == None, "header variable must be list or None"
+
         spreadsheet = {
             'properties': {
-                'title': sheet_name
+                'title': spreadsheet_name
             }
         }
         self.spreadsheet = self.service.spreadsheets().create(body=spreadsheet,
                                         fields='spreadsheetId').execute()
-
-        self.write_data(rows)
+        if header != None:
+            self.write_data(header)
 
         logger.info('Google Sheet is Created')
     
-    def write_data(self, row):
+    def write_data(self, row: List) -> None:
+        """
+            Writes row the Spreadsheet file.
+            
+            Args:
+                row(list): Data that writes the Spreadsheet file
+
+            Returns:
+                None
+        """
+
+        assert type(row) == list, "row variable must be list"
+
         values = [
             row
         ]
         body = {
             'values': values
         }
-        result = self.service.spreadsheets().values().append(
+        
+        self.service.spreadsheets().values().append(
             spreadsheetId=self.spreadsheet.get('spreadsheetId'), range="A1:F1",
             valueInputOption="USER_ENTERED", body=body).execute()
         
         logger.info(f'Write Data in `Markastok | Ürün Raporu`: {row}')
-
-if __name__ == '__main__':
-    google_sheet = GoogleSheet()
-    google_sheet.create_sheet("merhaba dünya")
-    for i in range(10):
-        google_sheet.write_data(["URL", "Product Code", "ProductName", "Availability", "Offer", "Product Price", "Sale Price"])
